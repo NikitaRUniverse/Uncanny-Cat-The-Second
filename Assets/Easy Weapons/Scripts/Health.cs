d029d4f1c5d59b8ebf7742f874dc6a582bccdb77
NikitaRUNI <cherkashin-nikita22@mail.ru>
2025-04-09 22:37:47 +0300

    Fixing explosions

--- Assets/Easy Weapons/Scripts/Health.cs
+++ Assets/Easy Weapons/Scripts/Health.cs
@@ -5,6 +5,10 @@ public class Health : MonoBehaviour
     public float startingHealth = 100.0f;
     public float maxHealth = 100.0f;
     private float currentHealth;
+    public GameObject explosion;                // The explosion prefab to be instantiated
+
+    public bool isPlayer = false;               // Whether or not this health is the player
+    public GameObject deathCam;					// The camera to activate when the player dies
 
     private void Start()
     {
@@ -34,12 +38,18 @@ public class Health : MonoBehaviour
 
     public void Die()
     {
+        if (isPlayer && deathCam != null)
+            deathCam.SetActive(true);
+
+        GetComponent<MeshRenderer>().enabled = false;
+        Instantiate(explosion, transform.position, transform.rotation);
+
         SwarmAgent agent = GetComponent<SwarmAgent>();
         if (agent != null)
         {
             agent.Die();
         }
-        else
+        else if (!isPlayer)
         {
             Destroy(gameObject);
         }
